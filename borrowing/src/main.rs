85f1dddc58faa7fe3d40fbdb2e5ea77986ef3c37
Webrowse <great.adarsh@gmail.com>
2024-07-29 02:21:29 +0530

    solve 3 exercise under borrowing

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -4,4 +4,29 @@ fn main() {
     let p = &x;
  
     println!("the memory address of x is {:p}", p); // One possible output: 0x16fa3ac84
- }
+ 
+    //2.
+
+    let x = 5;
+    let _y = &x;
+
+    // Modify this line only
+    assert_eq!(5, x);
+
+    println!("Success!2");
+
+//3.
+let mut s = String::from("hello, ");
+
+borrow_object(&s);
+
+println!("Success!3");
+
+//4.
+
+
+}
+//3.
+fn borrow_object(s: &String) {}
+
+
