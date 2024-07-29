6cc54b9d0a5f6c383574e1a80a1664a6e1ccecdb
Webrowse <great.adarsh@gmail.com>
2024-07-30 03:46:30 +0530

    9 exercise of borrowing done

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -19,7 +19,7 @@ fn main() {
 //3.
 let s = String::from("hello, ");
 
-borrow_object(&s);
+//borrow_object(&s);
 
 println!("Success!3");
 
@@ -39,11 +39,55 @@ let p = &mut s;
 p.push_str("world");
 
 println!("Success!5");
+//6.
+let c = '中';
+
+let r1 = &c;
+// Fill the blank，dont change other code
+let ref  r2 = c;
+
+assert_eq!(*r1, *r2);
+
+// Check the equality of the two address strings
+assert_eq!(get_addr(r1),get_addr(r2));
+
+println!("Success!6");
+//7.
+let s3 = String::from("hello");
+
+    let r1 = &s3;
+    let r2 = &s3;
+
+    println!("{}, {}", r1, r2);
+
+    println!("Success!7");
+//8.
+     // Fix error by modifying this line
+     let mut s2 = String::from("hello, ");
+
+     borrow_object(&mut s2);
+ 
+     println!("Success!8");
+//9.
+     let mut s = String::from("hello, ");
+
+    borrow_object1(&s);
+    
+    s.push_str("world");
+
+    println!("Success!9");
 }
 //3.
-fn borrow_object(_s: &String) {}
+//fn borrow_object(_s: &String) {}
 //4.
 fn push_str(s1: &mut String) {
     s1.push_str("world")
 }
-
+//6.
+fn get_addr(r: &char) -> String {
+    format!("{:p}", r)
+}
+//8.
+fn borrow_object(s2: &mut String) {}
+//9.
+fn borrow_object1(adarsh: &String) {}
