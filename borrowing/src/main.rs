487cf55336eb072ef207673563114bb43db6fa96
Webrowse <great.adarsh@gmail.com>
2024-07-31 21:36:50 +0530

    Finished Borrowing

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -76,6 +76,15 @@ let s3 = String::from("hello");
     s.push_str("world");
 
     println!("Success!9");
+//10.
+let mut s = String::from("hello, ");
+
+let r1 = &mut s;
+r1.push_str("world");
+let r2 = &mut s;
+r2.push_str("!");
+
+println!("{}",r2);
 }
 //3.
 //fn borrow_object(_s: &String) {}
