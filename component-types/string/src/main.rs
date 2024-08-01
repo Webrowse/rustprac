9fbede902d18f5adf5f0cbc5e2e856b4de198058
Webrowse <great.adarsh@gmail.com>
2024-08-01 23:38:37 +0530

    2 exercise of string and str

--- component-types/string/src/main.rs
+++ component-types/string/src/main.rs
@@ -1,3 +1,16 @@
+
+fn greetings(s: &str) {
+    println!("{}",s)
+}
 fn main() {
-    println!("Hello, world!");
+    let s: &str = "hello, world";
+
+    println!("Success!1");
+
+//2.
+    let s: Box<str> = "hello, world".into();
+    greetings(&s)
+
+
+
 }
