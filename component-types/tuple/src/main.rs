cd19cbd38217e6a682e81dbb49296e8239ee1e79
Webrowse <great.adarsh@gmail.com>
2024-08-06 14:55:41 +0530

    tuple finished

--- component-types/tuple/src/main.rs
+++ component-types/tuple/src/main.rs
@@ -28,7 +28,28 @@ assert_eq!(y, "hello");
 assert_eq!(z, 6.4);
 
 println!("Success!4");
+//5.
+let (x, y, z);
 
+// Fill the blank
+(y,z,x) = (1, 2, 3);
+
+assert_eq!(x, 3);
+assert_eq!(y, 1);
+assert_eq!(z, 2);
+
+println!("Success!5");
+
+//6.
+// Fill the blank, need a few computations here.
+let (x, y) = sum_multiply((2,3));
+
+assert_eq!(x, 5);
+assert_eq!(y, 6);
+
+println!("Success!6");
 
 }
-
+fn sum_multiply(nums: (i32, i32)) -> (i32, i32) {
+    (nums.0 + nums.1, nums.0 * nums.1)
+}
