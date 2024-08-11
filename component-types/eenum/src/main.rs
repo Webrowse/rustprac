d925cd5267c59aab44b0e2fb3d3640ad33f83497
Webrowse <great.adarsh@gmail.com>
2024-08-11 06:34:09 +0530

    enum initialized

---
+++ component-types/eenum/src/main.rs
@@ -0,0 +1,33 @@
+//1
+// Fix the errors
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+enum Number1 {
+    Zero = 0,
+    One,
+    Two,
+}
+
+// C-like enum
+enum Number2 {
+    Zero = 0,
+    One = 1,
+    Two = 2,
+}
+
+
+fn main() {
+    // An enum variant can be converted to a integer by `as`
+    assert_eq!(Number::One as u8, Number1::One as u8);
+    assert_eq!(Number1::One as isize, Number2::One as isize);
+
+    println!("Success!1");
+
+
+
+    
+} 
