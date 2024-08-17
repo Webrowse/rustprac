ce00a110ed88619e3de9511621333944f10e04b3
Webrowse <great.adarsh@gmail.com>
2024-08-17 07:56:18 +0530

    impl completed Ass.Func

--- method/src/main.rs
+++ method/src/main.rs
@@ -43,21 +43,80 @@
 
 //3.
 
-struct TrafficLight {
-    color: String,
+// struct TrafficLight {
+//     color: String,
+// }
+
+// impl TrafficLight {
+//     // Using `Self` to fill in the blank.
+//     pub fn show_state(&self)  {
+//         println!("the current state is {}", self.color);
+//     }
+
+//     // Fill in the blank, DON'T use any variants of `Self`.
+//     pub fn change_state(&mut self) {
+//         self.color = "green".to_string()
+//     }
+// }
+// fn main() {
+//     println!("Success!");
+// }
+
+//4.
+
+// #[derive(Debug)]
+// struct TrafficLight {
+//     color: String,
+// }
+
+// impl TrafficLight {
+//     // 1. Implement an associated function `new`,
+//     // 2. It will return a TrafficLight contains color "red"
+//     // 3. Must use `Self`, DONT use `TrafficLight` in fn signatures or body
+//     pub fn new() ->TrafficLight{
+//         TrafficLight{
+//             color : "red".to_string()
+//         }
+//     }
+
+//     pub fn get_state(&self) -> &str {
+//         &self.color
+//     }
+// }
+
+// fn main() {
+//     let light: TrafficLight = TrafficLight::new();
+//     assert_eq!(light.get_state(), "red");
+
+//     println!("Success!");
+// }
+
+//5.
+
+
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-impl TrafficLight {
-    // Using `Self` to fill in the blank.
-    pub fn show_state(&self)  {
-        println!("the current state is {}", self.color);
-    }
-
-    // Fill in the blank, DON'T use any variants of `Self`.
-    pub fn change_state(&mut self) {
-        self.color = "green".to_string()
+// Using multiple `impl` blocks to rewrite the code below.
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
 }
+impl Rectangle {
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
+
 fn main() {
     println!("Success!");
 }
+
+//6.
+
