5262ffb6a54e9f30a3d5f11d4a24054f503707b4
Webrowse <great.adarsh@gmail.com>
2024-08-19 23:59:15 +0530

    more generics exercise

--- genericTraits/generics/src/main.rs
+++ genericTraits/generics/src/main.rs
@@ -46,17 +46,33 @@
 //3.
 
 
-// Implement struct Point to make it work.
-struct Point<T> {
-    x:T,
-    y:T,
+// // Implement struct Point to make it work.
+// struct Point<T> {
+//     x:T,
+//     y:T,
     
+// }
+
+// fn main() {
+//     let integer = Point { x: 5, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+
+//     println!("Success!");
+
+// }
+
+//4.
+
+
+// Modify this struct to make the code work
+struct Point<T, S> {
+    x: T,
+    y: S,
 }
 
 fn main() {
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 1.0, y: 4.0 };
+    // DON'T modify this code.
+    let p = Point{x: 5, y : "hello".to_string()};
 
     println!("Success!");
-
 }
