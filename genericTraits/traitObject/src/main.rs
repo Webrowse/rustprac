7ebd1297523168d5e4a67bcb27e104a6a578a60f
Webrowse <great.adarsh@gmail.com>
2024-08-26 23:35:04 +0530

    static and dynamic dispatch complete

--- genericTraits/traitObject/src/main.rs
+++ genericTraits/traitObject/src/main.rs
@@ -62,45 +62,120 @@
 //2.
 //Array with trait objects
 
-trait Bird {
-    fn quack(&self);
+// trait Bird {
+//     fn quack(&self);
+// }
+
+// struct Duck;
+// impl Duck {
+//     fn fly(&self) {
+//         println!("Look, the duck is flying")
+//     }
+// }
+// struct Swan;
+// impl Swan {
+//     fn fly(&self) {
+//         println!("Look, the duck.. oh sorry, the swan is flying")
+//     }
+// }
+
+// impl Bird for Duck {
+//     fn quack(&self) {
+//         println!("{}", "duck duck");
+//     }
+// }
+
+// impl Bird for Swan {
+//     fn quack(&self) {
+//         println!("{}", "swan swan");
+//     }
+// }
+
+// fn main() {
+//     // FILL in the blank to make the code work.
+//     let birds: [&dyn Bird; 2] = [&Duck, &Swan];  //usize
+
+//     for bird in birds {
+//         bird.quack();
+//         // When duck and swan turn into Birds, they all forgot how to fly, only remember how to quack.
+//         // So, the code below will cause an error.
+//         // bird.fly();
+//     }
+// }
+
+//3.
+//&dyn and Box<dyn>
+
+// // FILL in the blanks.
+// trait Draw {
+//     fn draw(&self) -> String;
+// }
+
+// impl Draw for u8 {
+//     fn draw(&self) -> String {
+//         format!("u8: {}", self)
+//     }
+// }
+
+// impl Draw for f64 {
+//     fn draw(&self) -> String {
+//         format!("f64: {}", self)
+//     }
+// }
+
+// fn main() {
+//     let x: f64 = 1.1f64;
+//     let y: u8 = 8u8;
+
+//     // Draw x.
+//     draw_with_box(Box::new(x));
+
+//     // Draw y.
+//     draw_with_ref(&y);
+
+//     println!("Success!");
+// }
+
+// fn draw_with_box(x: Box<dyn Draw>) {
+//     x.draw();
+// }
+
+// fn draw_with_ref(x: &dyn Draw) {
+//     x.draw();
+// }
+
+//4.
+//Static and Dynamic dispatch
+
+
+trait Foo {
+    fn method(&self) -> String;
 }
 
-struct Duck;
-impl Duck {
-    fn fly(&self) {
-        println!("Look, the duck is flying")
-    }
-}
-struct Swan;
-impl Swan {
-    fn fly(&self) {
-        println!("Look, the duck.. oh sorry, the swan is flying")
-    }
+impl Foo for u8 {
+    fn method(&self) -> String { format!("u8: {}", *self) }
 }
 
-impl Bird for Duck {
-    fn quack(&self) {
-        println!("{}", "duck duck");
-    }
+impl Foo for String {
+    fn method(&self) -> String { format!("string: {}", *self) }
 }
 
-impl Bird for Swan {
-    fn quack(&self) {
-        println!("{}", "swan swan");
-    }
+// IMPLEMENT below with generics.
+fn static_dispatch<T: Foo>(a:T) {
+    a.method();
+}
+
+// Implement below with trait objects.
+fn dynamic_dispatch(a: &dyn Foo){
+    a.method();
 }
 
 fn main() {
-    // FILL in the blank to make the code work.
-    let birds: [&dyn Bird; 2] = [&Duck, &Swan];  //usize
+    let x :u8= 5u8;
+    let y : String= "Hello".to_string();
 
-    for bird in birds {
-        bird.quack();
-        // When duck and swan turn into Birds, they all forgot how to fly, only remember how to quack.
-        // So, the code below will cause an error.
-        // bird.fly();
-    }
+    static_dispatch(x);
+    dynamic_dispatch(&y);
+
+    println!("Success!");
 }
-
-
