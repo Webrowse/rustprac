6d4988eb4b1b27041a7bffe8eb8db2ddde7e13db
Webrowse <great.adarsh@gmail.com>
2024-08-31 23:41:31 +0530

    solve vec issue

--- collectionTypes/vectorCollection/src/main.rs
+++ collectionTypes/vectorCollection/src/main.rs
@@ -1,29 +1,31 @@
-
 fn main() {
     let arr: [u8; 3] = [1, 2, 3];
     
+    // Convert array to Vec<u8>
     let v: Vec<u8> = Vec::from(arr);
     is_vec(&v);
 
+    // Create a Vec<u8> directly
     let v: Vec<u8> = vec![1, 2, 3];
     is_vec(&v);
 
-    // vec!(..) and vec![..] are same macros, so
+    // vec!(..) is the same as vec![..]
     let v: Vec<u8> = vec!(1, 2, 3);
-    is_vec(&v.clone());
-    
-    // In code below, v is Vec<[u8; 3]> , not Vec<u8>
-    // USE Vec::new and `for` to rewrite the below code 
-    let mut v1:Vec<T>::new();
-    is_vec(v1.clone());
-    for i in &v{
-        v1.push(*i);  // clone the value to avoid borrowing error  // or use v.clone() instead of *i  // or use &i instead of *i to avoid borrowing error  // or use &v[i] instead of *i to avoid borrowing error  // or use &v.clone()[i] instead of *i to avoid borrowing error  // or use v.iter().cloned().collect::<Vec<_>>() instead of v.clone() to avoid borrowing error  // or use v.iter().cloned().collect::<Vec<_>>() instead of *i to avoid borrowing error  // or use v.iter().cloned().collect::<Vec<_>>() instead of *i to avoid borrowing error  // or use v.iter().cloned().collect::<Vec<_>>() instead of *i to avoid borrowing error  // or use v.iter().cloned().collect::<Vec<_>>() instead of *i to
-    }
-    
- 
-    assert_eq!(v, v1);
+    is_vec(&v);  // Correctly pass a reference
+
+    // Using Vec::new and `for` to populate a Vec of arrays
+    let mut v1: Vec<[u8; 3]> = Vec::new();
+    v1.push(arr);
+
+    // Convert the first element (array) to Vec<u8> before passing to is_vec
+    is_vec(&v1[0].to_vec());
+
+    // Convert `v1[0]` to Vec<u8> to compare with `v`
+    assert_eq!(v, v1[0].to_vec());
 
     println!("Success!");
 }
 
+// Function that accepts a reference to a Vec<u8>
 fn is_vec(v: &Vec<u8>) {}
+
