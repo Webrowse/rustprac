2fec2164cdbd90b3a6193dcc0c8f40e7224648e7
Webrowse <great.adarsh@gmail.com>
2024-09-02 04:28:40 +0530

    hashmap finished

--- collectionTypes/hashmapColection/src/main.rs
+++ collectionTypes/hashmapColection/src/main.rs
@@ -1,27 +1,142 @@
 
-// FILL in the blanks and FIX the errors
+// // FILL in the blanks and FIX the errors
+// use std::collections::HashMap;
+// fn main() {
+//     let mut scores: HashMap<&str, i32> = HashMap::new();
+//     scores.insert("Sunface", 98);
+//     scores.insert("Daniel", 95);
+//     scores.insert("Ashley", 69);
+//     scores.insert("Katie", 58);
+
+//     // Get returns an Option<&V>
+//     let score: Option<&i32> = scores.get("Sunface");
+//     assert_eq!(score, Some(&98));
+
+//     if scores.contains_key("Daniel") {
+//         // Indexing returns a value V
+//         let score = scores["Daniel"];
+//         assert_eq!(score, 95);
+//         scores.remove("Daniel");
+//     }
+
+//     assert_eq!(scores.len(), 3);
+
+//     for (name, score) in scores {
+//         println!("The score of {} is {}", name, score);
+//     }
+// }
+
+
+// use std::collections::HashMap;
+// fn main() {
+//     let teams: [(&str, i32);3] = [
+//         ("Chinese Team", 100),
+//         ("American Team", 10),
+//         ("France Team", 50),
+//     ];
+
+//     let mut teams_map1 = HashMap::new();
+//     for team in &teams {
+//         teams_map1.insert(team.0, team.1);
+//     }
+
+//     // IMPLEMENT team_map2 in two ways
+//     // Tips: one of the approaches is to use `collect` method
+//     //let teams_map2: HashMap<&str, i32> = HashMap::from(teams);
+
+//     let teams_map2: HashMap<&str, i32> = teams.into_iter().collect();
+//     assert_eq!(teams_map1, teams_map2);
+
+//     println!("Success!");
+// }
+
+
+// // FILL in the blanks
+// use std::collections::HashMap;
+// fn main() {
+//     // Type inference lets us omit an explicit type signature (which
+//     // would be `HashMap<&str, u8>` in this example).
+//     let mut player_stats = HashMap::new();
+
+//     // Insert a key only if it doesn't already exist
+//     player_stats.entry("health").or_insert(100);
+
+//     assert_eq!(player_stats["health"], 100);
+
+//     // Insert a key using a function that provides a new value only if it
+//     // doesn't already exist
+//     player_stats.entry("health").or_insert_with(random_stat_buff);
+//     assert_eq!(player_stats["health"], 100);
+
+//     // Ensures a value is in the entry by inserting the default if empty, and returns
+//     // a mutable reference to the value in the entry.
+//     let health = player_stats.entry("health").or_insert(50);
+//     assert_eq!(health, &100);
+//     *health -= 50;
+//     assert_eq!(*health, 50);
+
+//     println!("Success!");
+// }
+
+// fn random_stat_buff() -> u8 {
+//     // Could actually return some random value here - let's just return
+//     // some fixed value for now
+//     42
+// }
+
+//4
+
+
+// // FIX the errors
+// // Tips: `derive` is usually a good way to implement some common used traits
+// use std::collections::HashMap;
+// #[derive(Debug, PartialEq, Eq, Hash)]
+// struct Viking {
+//     name: String,
+//     country: String,
+// }
+
+// impl Viking {
+//     /// Creates a new Viking.
+//     fn new(name: &str, country: &str) -> Viking {
+//         Viking {
+//             name: name.to_string(),
+//             country: country.to_string(),
+//         }
+//     }
+// }
+
+// fn main() {
+//     // Use a HashMap to store the vikings' health points.
+//     let vikings = HashMap::from([
+//         (Viking::new("Einar", "Norway"), 25),
+//         (Viking::new("Olaf", "Denmark"), 24),
+//         (Viking::new("Harald", "Iceland"), 12),
+//     ]);
+
+//     // Use derived implementation to print the status of the vikings.
+//     for (viking, health) in &vikings {
+//         println!("{:?} has {} hp", viking, health);
+//     }
+// }
+
+//5.
+
+// FIX the errors with least changes
+// DON'T remove any code line
 use std::collections::HashMap;
 fn main() {
-    let mut scores: HashMap<&str, i32> = HashMap::new();
-    scores.insert("Sunface", 98);
-    scores.insert("Daniel", 95);
-    scores.insert("Ashley", 69);
-    scores.insert("Katie", 58);
+  let v1 = 10;
+  let mut m1 = HashMap::new();
+  m1.insert(v1, v1);
+  println!("v1 is still usable after inserting to hashmap : {}", v1);
 
-    // Get returns an Option<&V>
-    let score: Option<&i32> = scores.get("Sunface");
-    assert_eq!(score, Some(&98));
+  let v2 = "hello".to_string();
+  let mut m2 = HashMap::new();
+  // Ownership moved here
+  m2.insert(&v2, v1);
+    
+  assert_eq!(v2, "hello");
 
-    if scores.contains_key("Daniel") {
-        // Indexing returns a value V
-        let score = scores["Daniel"];
-        assert_eq!(score, 95);
-        scores.remove("Daniel");
-    }
-
-    assert_eq!(scores.len(), 3);
-
-    for (name, score) in scores {
-        println!("The score of {} is {}", name, score);
-    }
+  println!("Success!");
 }
