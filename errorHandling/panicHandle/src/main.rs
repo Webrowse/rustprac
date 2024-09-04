bbfb55f0b93293e10f7ee52f2c3988ab66e763b3
Webrowse <great.adarsh@gmail.com>
2024-09-04 23:59:08 +0530

    panic started

---
+++ errorHandling/panicHandle/src/main.rs
@@ -0,0 +1,17 @@
+
+// FILL the blanks
+fn drink(beverage: &str) {
+    if beverage == "lemonade" {
+        println!("Success!");
+        // IMPLEMENT the below code
+        panic!()
+     }
+
+    println!("Exercise Failed if printing out this line!");
+}
+
+fn main() {
+    drink("kemonade");
+
+    println!("Exercise Failed if printing out this line!");
+}
