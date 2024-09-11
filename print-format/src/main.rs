3fa57aa8c04e36530d76666b6edfdb989b182031
Webrowse <great.adarsh@gmail.com>
2024-09-11 23:38:30 +0530

    print+format type

---
+++ print-format/src/main.rs
@@ -0,0 +1,31 @@
+
+// fn main() {
+//     let s1 = "hello";
+//     /* Fill in the blank */
+//     let s = format!("{}, world!", s1);
+//     assert_eq!(s, "hello, world!");
+// }
+
+
+fn main() {
+    /* Fill in the blanks to make it print:
+    Hello world, I am 
+    Sunface!
+    */
+    print!("hello world, ");
+    println!("I am");
+    print!("Sunface!");
+ }
+
+//Debug traits 
+
+
+// /* Fill in the blanks and Fix the errors */
+// struct Structure(i32);
+
+// fn main() {
+//     // Types in std and Rust have implemented the fmt::Debug trait
+//     println!("__ months in a year.", 12);
+
+//     println!("Now __ will print!", Structure(3));
+// }
