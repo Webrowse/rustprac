1d4e90ed0603c284e64cbf80618c25a6c72b66b0
Webrowse <great.adarsh@gmail.com>
2024-09-11 23:52:44 +0530

    debug complete

--- print-format/src/main.rs
+++ print-format/src/main.rs
@@ -7,25 +7,69 @@
 // }
 
 
-fn main() {
-    /* Fill in the blanks to make it print:
-    Hello world, I am 
-    Sunface!
-    */
-    print!("hello world, ");
-    println!("I am");
-    print!("Sunface!");
- }
+// fn main() {
+//     /* Fill in the blanks to make it print:
+//     Hello world, I am 
+//     Sunface!
+//     */
+//     print!("hello world, ");
+//     println!("I am");
+//     print!("Sunface!");
+//  }
 
 //Debug traits 
 
 
 // /* Fill in the blanks and Fix the errors */
+// #[derive(Debug)]
 // struct Structure(i32);
 
 // fn main() {
 //     // Types in std and Rust have implemented the fmt::Debug trait
-//     println!("__ months in a year.", 12);
+//     println!("{} months in a year.", 12);
 
-//     println!("Now __ will print!", Structure(3));
+//     println!("Now {:?} will print!", Structure(3));
 // }
+
+
+
+// #[derive(Debug)]
+// struct Person {
+//     name: String,
+//     age: u8
+// }
+
+// fn main() {
+//     let person = Person { name:  "Sunface".to_string(), age: 18 };
+
+//     /* Make it output: 
+//     Person {
+//         name: "Sunface",
+//         age: 18,
+//     }
+//     */
+//     println!("{:#?}", person);
+// }
+
+
+
+
+use core::fmt;
+struct Structure(i32);
+
+
+struct Deep(Structure);
+
+impl fmt::Debug for Deep {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0.0)
+    }
+
+}
+fn main() {    
+    // The problem with `derive` is there is no control over how
+    // the results look. What if I want this to just show a `7`?
+
+    /* Make it print: Now 7 will print! */
+    println!("Now {:?} will print!", Deep(Structure(7)));
+}
